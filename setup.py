c1d116f9592e4ec6a0b2ea6a283029125508a903
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-09-28 14:19:29 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    # long_description=long_description,
+    packages=setuptools.find_packages(),
+    install_requires = [
+        "",
+    ]
+)
