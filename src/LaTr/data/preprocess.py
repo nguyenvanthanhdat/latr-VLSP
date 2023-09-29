18cba6c4ed1427faa0e34f9610e1845a0413cd67
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-09-29 21:39:06 +0700

    Feat: check file name in dataframe exist or not

--- src/LaTr/data/preprocess.py
+++ src/LaTr/data/preprocess.py
@@ -2,34 +2,51 @@ from src.LaTr.config.config import ConfigurationManager
 import json, os
 import pandas as pd
 import copy
+from tqdm.auto import tqdm
 
 config_data = ConfigurationManager().config.data
 # print(config_data)
+tqdm.pandas()
 
 
 def read2df(split):
     # data_path = config_data.split
     # dict_data = config_data.to_dict()
     dict_data = split.to_dict()
+    dfs = {}
     for type_dataset in dict_data:
-        temp = dict_data[type_dataset]
-        temp = pd.DataFrame(json.load(open(temp))['data'])
+        temp = pd.DataFrame(json.load(open(dict_data[type_dataset]))['data'])
     # dict_data['question'] = pd.DataFrame(json.load(open(dict_data['question']))['data'])
     # print(dict_data)
         if type_dataset == 'question':
             temp['answers'] = temp['answers'].apply(
                 lambda x: " ".join(list(map(str, x))))
         # print(type(temp))
-    return temp
+        dfs[type_dataset] = temp
+    return dfs
 
 dict_data = {
     'train': read2df(config_data.train),
     'val': read2df(config_data.val)
 }
 
-print(type(dict_data['train']['question']))
-# dict_data['train']['question']['path_exists'] = dict_data['train']['question']['image_id'].progress_apply(
-#     lambda x: os.path.exists(os.path.join(config_data.image_path, x)+'.jpg'))
+# check file name in dataframe exist or not
+## train dataframe
+dict_data['train']['question']['path_exists'] = dict_data[
+    'train']['question']['image_id'].progress_apply(
+    lambda x: os.path.exists(os.path.join(config_data.image_path, x)+'.jpg'))
+
+dict_data['train']['question'] = dict_data['train']['question'][
+    dict_data['train']['question']['path_exists']==True]
+
+## validation dataframe
+dict_data['val']['question']['path_exists'] = dict_data[
+    'val']['question']['image_id'].progress_apply(
+    lambda x: os.path.exists(os.path.join(config_data.image_path, x)+'.jpg'))
+
+dict_data['val']['question'] = dict_data['val']['question'][
+    dict_data['val']['question']['path_exists']==True]
+
 
 # base_img_path = os.path.join('drive/MyDrive/TextVQA', 'train_images')
 # train_json_df['path_exists'] = train_json_df['image_id'].progress_apply(
